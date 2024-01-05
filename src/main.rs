af44a68ce45c22aee3fb303da429519828cdd5e6
Kevin Németh <nkevin@outlook.hu>
2024-01-05 17:15:40 +0100

    Add axum and tokio dependencies, and update main.rs to include a basic health check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health_check", get(health_check));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn health_check() {}
