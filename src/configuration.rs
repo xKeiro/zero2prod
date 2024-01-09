ac77dd6ac814e28b1cea16e7438720b19b6deacc
Kevin Németh <nkevin@outlook.hu>
2024-01-09 12:26:08 +0100

    Add connection_string method to DatabaseSettings

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,16 +12,21 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-// Initialise our configuration reader
-let settings = config::Config::builder()
-// Add configuration values from a file named `configuration.yaml`.
-.add_source(
-config::File::new("configuration.yaml", config::FileFormat::Yaml)
-)
-.build()?;
-// Try to convert the configuration values it read into
-// our Settings type
-settings.try_deserialize::<Settings>()
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    settings.try_deserialize::<Settings>()
+}
