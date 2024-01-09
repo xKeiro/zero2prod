b73c558790e1cdfec67142c57d053b65881abef3
Kevin Németh <nkevin@outlook.hu>
2024-01-09 11:28:19 +0100

    Add prerequisites check and Postgres availability check in init_db.sh script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -4,6 +4,22 @@
 set -x
 # Make the shell exit if any invoked command fails
 set -eo pipefail
+
+# Check if prerequisites are istalled
+if ! [ -x "$(command -v psql)" ]; then
+echo >&2 "Error: psql is not installed."
+exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+echo >&2 "Error: sqlx is not installed."
+echo >&2 "Use:"
+echo >&2 " cargo install --version='~0.7' sqlx-cli \
+--no-default-features --features rustls,postgres"
+echo >&2 "to install it."
+exit 1
+fi
+
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
@@ -23,3 +39,16 @@ docker run \
 -d postgres \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
+
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+>&2 echo "Postgres is still unavailable - sleeping"
+sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
